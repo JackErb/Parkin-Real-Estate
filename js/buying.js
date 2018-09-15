585a075d419add32028ab6c3292943dcba3831b8
JackErb <ankhenac@gmail.com>
2018-09-15 10:49:40 -0700

    Adds listing section to "buying" page

--- js/buying.js
+++ js/buying.js
@@ -1,5 +1,9 @@
-$(window).resize(function() {
+$(window).resize(resizeVideos);
+
+$(window).ready(resizeVideos);
+
+function resizeVideos() {
   var video = $('#coeurdalene-video');
   console.log(video.width() * 622 / 1110);
   video.height(video.width() * 622 / 1110);
-});
+}
