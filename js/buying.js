64054126db6c49a959a294d436512c2f8a906297
JackErb <ankhenac@gmail.com>
2018-09-15 13:36:10 -0700

    Fixes bug in resizing code

--- js/buying.js
+++ js/buying.js
@@ -1,9 +1,8 @@
 $(window).resize(resizeVideos);
 
-$(window).ready(resizeVideos);
+$(document).ready(resizeVideos);
 
 function resizeVideos() {
   var video = $('#coeurdalene-video');
-  console.log(video.width() * 622 / 1110);
   video.height(video.width() * 622 / 1110);
 }
